f1ab2cdb2df198d8b79f4f6d8d6dbccfd4187a4b
AkAyush <jain.sk05@gmail.com>
2024-05-06 20:51:33 +0530

    chore(conduit): Generates patch.

--- create-patch.sh
+++ create-patch.sh
@@ -6,4 +6,4 @@ git commit --allow-empty -am "chore(conduit): Generates patch."
 
 # Generate the diff
 NAME=$(git config user.name | sed s/[^[:alnum:]+._-]//g)
-git diff origin/master...HEAD -- . ':!*.patch' ':!yarn.lock' ':!package-lock.json' ':!**/tsconfig*.json' > "submission_${NAME:-code}.patch"
+git diff b6fa5f078c312cc68e8c78634f1d3c7c5b23c491 5fe866a325d1d7b29297ae3aa659b3784400d005 -- . ':!*.patch' ':!yarn.lock' ':!package-lock.json' ':!**/tsconfig*.json' > "submission_${NAME:-code}.patch"
